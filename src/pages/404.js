d0515c7475972f62b2636497b5b538d8509e8474
apellanr <ryan.apellanes@gmail.com>
2019-08-10 11:07:19 -0700

    nathan pyle

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,28 @@
 import React from "react"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Img from "gatsby-image"
+
+export const query = graphql`
+  query {
+    file(relativePath: { eq: "nathan-pyle-employ.jpg" }) {
+      childImageSharp {
+        # Specify the image processing specifications right in the query.
+        # Makes it trivial to update as your page's design changes.
+        fixed(width: 125, height: 125) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+`
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
+    <Img fixed={query.file.childImageSharp.fixed} />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
